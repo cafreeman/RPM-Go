54f8ecb3be51b7674a2191d687f377755fe1002d
cafreeman <cfreeman@alteryx.com>
2015-09-25 17:01:14 -0500

    Refactor the iterative package install process
    Each package install is now its open separate command, which seems to fix the segfault problem I was running into before when trying to install a list of packages from a single command.

--- installRPackages.go
+++ installRPackages.go
@@ -11,28 +11,22 @@ func installRPackages(rootPath, installDir, version string) {
 	packageDir := packageRepoDir(rootPath, version)
 	dirContents, err := ioutil.ReadDir(packageDir)
 	errCheck(err)
-	// Initialize the cmdArgs slice with the two static arguments we need to pass to R.exe, then
-	// iterate over the directory contents and append all of the winbows binaries to cmdArgs
-	cmdArgs := []string{"CMD", "INSTALL"}
-	for _, elem := range dirContents {
-		// Extract filename from os.FileInfo struct
-		name := elem.Name()
-		// Filter out only the files that end in `.zip` (since these will be the Windows binaries), and
-		// append to the `filtered` slice
-		if filepath.Ext(name) == ".zip" {
-			fullPath := filepath.Join(packageDir, name)
-			cmdArgs = append(cmdArgs, fullPath)
-		}
-	}
 
 	// Construct path to R executable
 	rCMDPath := filepath.Join(convertToWindowsPath(installDir), "bin", "R.exe")
-	// Create Command struct with arguments from the cmdArgs slice
-	cmd := exec.Command(rCMDPath, cmdArgs...)
-	// Execute R CMD INSTALL for all packages and print the stdout
-	out, err := cmd.Output()
-	errCheck(err)
-	fmt.Println(string(out))
+
+	for _, elem := range dirContents {
+		name := elem.Name()
+		if filepath.Ext(name) == ".zip" {
+			fullpath := filepath.Join(packageDir, name)
+			cmdString := exec.Command(rCMDPath, "CMD", "INSTALL", fullpath)
+			out, err := cmdString.Output()
+			fmt.Println(string(out))
+			fmt.Println(err)
+			// cmdArgs = append(cmdArgs, cmdString)
+		}
+	}
+
 }
 
 func packageRepoDir(rootPath, version string) string {
